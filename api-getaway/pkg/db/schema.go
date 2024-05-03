7e939d03d17eb865054105a3a31238ac2608f6a9
nutcas3 <mauricee423@gmail.com>
2024-05-03 15:28:57 +0300

    create users query

--- api-getaway/pkg/db/schema.go
+++ api-getaway/pkg/db/schema.go
@@ -6,3 +6,23 @@ import (
 )
 
 var DB *sqlx.DB
+
+func CreateTables() error {
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			name VARCHAR(255) NOT NULL,
+			email VARCHAR(255) NOT NULL UNIQUE
+		);`,
+
+	}
+
+	for _, query := range queries {
+		_, err := DB.Exec(query)
+		if err != nil {
+			return fmt.Errorf("error executing query: %v", err)
+		}
+	}
+
+	return nil
+}
